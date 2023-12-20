12df31b5c9c1958df2de1ab64fc74a8f0c3e7818
Gethe <gethe88@gmail.com>
2023-12-20 01:08:10 -0500

    Add cleanup script

---
+++ common/scripts/clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+rm -rf kiauh
+rm -rf kiauh-backups
+rm -f .kiauh.ini
+
+rm -rf klipper
+rm -rf klippy-env
+
+rm -rf mainsail
+
+rm -rf moonraker
+rm -rf moonraker-env
+
+rm -rf printer_data
