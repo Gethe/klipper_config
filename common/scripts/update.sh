af154f2c296f3f4ed5ea919cebfc05d6db3de9c8
Gethe <gethe88@gmail.com>
2024-01-15 08:30:10 -0500

    Tweak status messages

--- common/scripts/update.sh
+++ common/scripts/update.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+# shellcheck source=./common/scripts/header.sh
+source "$HOME"/custom_config/common/scripts/header.sh
+
+status_msg "Applying config updates..."
+
+ok_msg "Finished updates!"
