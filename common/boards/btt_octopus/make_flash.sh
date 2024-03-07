2c371433e4c2954ac184d3cf1df71fc9bc61e6b0
Gethe <gethe88@gmail.com>
2024-03-06 22:41:34 -0500

    Tweak flash scripts

--- common/boards/btt_octopus/make_flash.sh
+++ common/boards/btt_octopus/make_flash.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-cp -f "$CONFIG_DIR"/common/boards/btt_octopus/firmware.config /home/pi/klipper/.config
-cd /home/pi/klipper || exit
+cp -f "$CONFIG_DIR"/common/boards/btt_octopus/firmware.config ~/klipper/.config
+
+pushd ~/klipper || exit
 make olddefconfig
 make clean
 make
 
+service klipper stop
 make flash FLASH_DEVICE=/dev/btt-octopus-11
+popd || exit
--- ruby/flash_mcu.sh
+++ ruby/flash_mcu.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# shellcheck source=./common/scripts/header.sh
+source "$HOME"/custom_config/common/scripts/header.sh
+
 "$CONFIG_DIR"/common/boards/btt_octopus/make_flash.sh
